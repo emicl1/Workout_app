8b1e886afde5a7b7568ca82c88a3e6c064ff55ad
Alex <ale.olivier.michaud@gmail.com>
2023-01-01 18:53:16 +0100

    Login and register systems are now functioning. Now I need to fix problem with navigation drawer

--- app.py
+++ app.py
@@ -1,27 +1,85 @@
 from kivy.lang import Builder
-
+from kivy.properties import StringProperty
+from kivy.uix.image import Image
 from kivymd.app import MDApp
+from kivymd.toast import toast
+from kivymd.uix.list import ILeftBody, OneLineAvatarListItem
 
-KV = '''
-MDBoxLayout:
-    orientation: "vertical"
-
- 
-
-    MDLabel:
-        text: "Content"
-        halign: "center"
-        
-    MDTopAppBar:
-        title: "MDTopAppBar"
-        right_action_items: [["dots-vertical", lambda x: app.callback_1()], ["clock", lambda x: app.callback_2()]]
-        "
-'''
+root_kv = """
+#:import NavigationLayout kivymd.uix.navigationdrawer.NavigationLayout
 
 
-class Test(MDApp):
+<ContentNavigationDrawer@MDNavigationDrawer>
+    drawer_logo: "demos/kitchen_sink/assets/drawer_logo.png"
+
+    NavigationDrawerSubheader:
+        text: "Menu:"
+
+
+<CustomNavigationDrawerIconButton>
+
+    AvatarSampleWidget:
+        source: root.source
+
+
+NavigationLayout:
+    id: nav_layout
+
+    ContentNavigationDrawer:
+        id: nav_drawer
+
+    BoxLayout:
+        orientation: "vertical"
+
+        MDToolbar:
+            id: toolbar
+            title: app.title
+            md_bg_color: app.theme_cls.primary_color
+            background_palette: "Primary"
+            background_hue: "500"
+            elevation: 10
+            left_action_items:
+                [["menu", lambda x: app.root.toggle_nav_drawer()]]
+
+        BoxLayout:
+            id: content
+            orientation: "vertical"
+"""
+
+
+class AvatarSampleWidget(ILeftBody, Image):
+    pass
+
+
+class CustomNavigationDrawerIconButton(OneLineAvatarListItem):
+    source = StringProperty()
+
+    def _set_active(self, active, nav_drawer):
+        pass
+
+
+class MainApp(MDApp):
+    def __init__(self, **kwargs):
+        self.title = "KivyMD Examples - Navigation Drawer with Custom Buttons"
+        self.theme_cls.primary_palette = "Orange"
+        super().__init__(**kwargs)
+
     def build(self):
-        return Builder.load_string(KV)
+        self.root = Builder.load_string(root_kv)
+
+    def on_start(self):
+        for i in range(15):
+            self.root.ids.nav_drawer.add_widget(
+                CustomNavigationDrawerIconButton(
+                    text=f"Item {i}",
+                    source="data/logo/kivy-icon-128.png",
+                    on_press=lambda x, y=i: self.callback(x, y),
+                )
+            )
+
+    def callback(self, instance, value):
+        toast("Pressed item menu %d" % value)
 
 
-Test().run()
+if __name__ == "__main__":
+    MainApp().run()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sqlite3
+import hashlib
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
 from kivy.lang import Builder
@@ -5,18 +7,180 @@ from kivy.core.window import Window
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.lang import Builder
 from kivymd.uix.textfield import MDTextFieldRect, MDTextField
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 
 Window.size = (300, 500)
 
 
+class FirstScreen(Screen):
+    pass
 
 
-class Example(MDApp):
+class LoginScreen(Screen):
+    pass
+
+
+class SignupScreen(Screen):
+    pass
+
+
+class ClosestWorkoutScreen(Screen):
+    pass
+
+
+class ThisWeekWorkoutScreen(Screen):
+    pass
+
+
+class ProfileScreen(Screen):
+    pass
+
+
+
+class WorkoutApp(MDApp):
+    dialog = None
     def build(self):
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        c.execute(
+            'CREATE TABLE IF NOT EXISTS users (username text, password text, bench integer, squat integer, deadlift integer)')
+        c.execute("CREATE TABLE IF NOT EXISTS now_logged (username text, password text)")
+        conn.commit()
+        conn.close()
         self.theme_cls.theme_style = "Dark"
-        return Builder.load_file("login.kv")
+        self.theme_cls.primary_palette = "DeepOrange"
 
-    def callback(self):
-        print("callback")
+        Builder.load_file("my.kv")
 
-Example().run()
+        sm = ScreenManager()
+        sm.add_widget(FirstScreen(name='first'))
+        sm.add_widget(LoginScreen(name='login'))
+        sm.add_widget(SignupScreen(name='signup'))
+        sm.add_widget(ClosestWorkoutScreen(name='closest_workout'))
+        sm.add_widget(ThisWeekWorkoutScreen(name='this_week_workout'))
+        sm.add_widget(ProfileScreen(name='profile'))
+
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        c.execute('SELECT username, password FROM now_logged')
+        data = c.fetchone()
+        print(data)
+        if data:
+            print("here")
+
+            sm.current = 'profile'
+        else:
+            sm.current = 'first'
+        conn.commit()
+        conn.close()
+        return sm
+
+
+    def login(self, username, password):
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        password = hashlib.sha256(password.encode()).hexdigest()
+        c.execute('SELECT username, password FROM users WHERE username = ? AND password = ?', (username, password))
+        data = c.fetchone()
+
+        if data:
+            self.root.current = 'profile'
+        else:
+            self.root.current = 'login'
+        c.execute("CREATE TABLE IF NOT EXISTS now_logged (username text, password text)")
+        c.execute("INSERT INTO now_logged VALUES (?, ?)", (username, password))
+        conn.commit()
+        conn.close()
+
+
+
+    def signup(self, username, password, password2, bench, squat, deadlift):
+        if password != password2:
+            if not self.dialog:
+                self.dialog = MDDialog(
+                    title="Error",
+                    text="Passwords do not match",
+                    size_hint=(0.8, 1),
+                    buttons=[
+                        MDFlatButton(
+                            text="Close", text_color=self.theme_cls.primary_color, on_release=self.close_dialog
+                        ),
+                    ],
+                )
+            self.dialog.open()
+        try:
+            bench = int(bench)
+            squat = int(squat)
+            deadlift = int(deadlift)
+        except ValueError:
+            if not self.dialog:
+                self.dialog = MDDialog(
+                    title="Error",
+                    text="Lifts must be numbers only",
+                    size_hint=(0.8, 1),
+                    buttons=[
+                        MDFlatButton(
+                            text="Close", text_color=self.theme_cls.primary_color, on_release=self.close_dialog
+                        ),
+                    ],
+                )
+            self.dialog.open()
+        if username == '' or password == '' or password2 == '' or bench == '' or squat == '' or deadlift == '':
+            if not self.dialog:
+                self.dialog = MDDialog(
+                    title="Error",
+                    text="Please fill out all fields",
+                    size_hint=(0.8, 1),
+                    buttons=[
+                        MDFlatButton(
+                            text="Close", text_color=self.theme_cls.primary_color, on_release=self.close_dialog
+                        ),
+                    ],
+                )
+            self.dialog.open()
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        c.execute('SELECT username FROM users WHERE username = ?', (username,))
+        data = c.fetchone()
+        if data:
+            if not self.dialog:
+                self.dialog = MDDialog(
+                    title="Error",
+                    text="Username already exists",
+                    size_hint=(0.8, 1),
+                    buttons=[
+                        MDFlatButton(
+                            text="Close", text_color=self.theme_cls.primary_color, on_release=self.close_dialog
+                        ),
+                    ],
+                )
+            self.dialog.open()
+
+        else:
+            conn = sqlite3.connect('users.db')
+            password = hashlib.sha256(password.encode()).hexdigest()
+            c = conn.cursor()
+            c.execute('INSERT INTO users VALUES (?, ?, ?, ?, ?)', (username, password, bench, squat, deadlift))
+            conn.commit()
+            conn.close()
+            self.root.current = 'login'
+
+    def close_dialog(self, obj):
+        self.dialog.dismiss()
+        self.dialog = None
+
+    def change_root(self, screen):
+        self.root.current = screen
+
+    def logout(self):
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        c.execute('DROP TABLE now_logged')
+        conn.commit()
+        conn.close()
+        self.root.current = 'first'
+
+
+WorkoutApp().run()
